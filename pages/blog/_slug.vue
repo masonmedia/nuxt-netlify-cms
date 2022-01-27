a5f47e677968ed031bc402e5f730e90823aff58e
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2022-01-26 22:31:38 -0500

    Update _slug

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,22 +1,33 @@
 <template>
-  <b-container fluid class="p-0">
-    <b-jumbotron 
-    class="d-flex flex-column justify-content-center align-items-start mb-0 px-5" 
-    style="min-height: 500px">
-      <h1 class="display-1">{{ article.title }}</h1>
-      <p class="h4 font-weight-light">{{ article.description }}</p>
-    </b-jumbotron>
+  <div>
+    <nav-component></nav-component>
+    <b-container fluid class="p-0">
+      <b-jumbotron
+        class="
+          d-flex
+          flex-column
+          justify-content-center
+          align-items-start
+          mb-0
+          px-5
+        "
+        style="min-height: 500px"
+      >
+        <h1 class="display-1">{{ article.title }}</h1>
+        <p class="h4 font-weight-light">{{ article.description }}</p>
+      </b-jumbotron>
 
-    <b-row class="p-5 bg-light" align-v="center">
-      <b-col lg="12">
-        <!-- <h1>{{ article.title }}</h1>
+      <b-row class="p-5 bg-light" align-v="center">
+        <b-col lg="12">
+          <!-- <h1>{{ article.title }}</h1>
         <p>{{ article.description }}</p> -->
-        <!-- <img style="position: absolute; z-index: 0; top: 0; left: 0" :src="article.img" :alt="article.alt" /> -->
-        <p>Post last updated: {{ formatDate(article.updatedAt) }}</p>
-        <nuxt-content :document="article" />
-      </b-col>
-    </b-row>
-  </b-container>
+          <!-- <img style="position: absolute; z-index: 0; top: 0; left: 0" :src="article.img" :alt="article.alt" /> -->
+          <p>Post last updated: {{ formatDate(article.updatedAt) }}</p>
+          <nuxt-content :document="article" />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
